8f6fd25dac64c673b7bbd7d3fa791f6364f9a531
MathyasYazew <88359082+MathyasYazew@users.noreply.github.com>
2022-01-30 19:18:33 +0300

    Update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,82 +1,65 @@
 #!/usr/bin/python3
 """
-    file storage modules
+File storage class
 """
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.amenity import Amenity
+import os
+from datetime import datetime
 
 
-class FileStorage:
-    """
-        File Storage Class
-    """
-    __file_path = "file.json"
+class FileStorage():
+    """File Storage class"""
+    __file_path = 'file.json'
     __objects = {}
 
+    def __init__(self):
+        """initializes instance"""
+        pass
+
     def all(self):
-        """
-        since __object is private class attribute
-        we access it like so
-        :return:
-        """
-        return self.__objects
+        """returns the dictionary '__objects'"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """
-        we add the object to the __objects attribute
-        the key is generated as <obj class name>.id
-        :obj : can have multi type class
-        :return: None
-        """
-        # att of that obj/instance
-        dict_attrs = obj.to_dict()
-        # generate some thing like "BaseModel.135489484231"
-        new_key = "{}.{}".format(dict_attrs["__class__"], dict_attrs["id"])
-        # in the class att we append that obj
-        self.__objects[new_key] = obj
-        # print("from new", FileStorage.__objects)
+        """sets in __objects the obj with key <obj class name>.id"""
+        obj_key = type(obj).__name__ + '.' + obj.id
+        FileStorage.__objects[obj_key] = obj
 
     def save(self):
-        """
-            each object/instance in the private class att __objects
-            we serialise the instance (by creating a newDict of
-            key and dict of attrs)
-            then write the newDict to the file.json
-        """
-        json_obj = {}
-        for key in self.__objects:
-            json_obj[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as file:
-            json.dump(json_obj, file, indent=2)
+        """serializes __objects to the JSON file (path: __file_path)"""
+        if os.path.exists(FileStorage.__file_path):
+            new_data = dict((k, v.to_dict())
+                            for k, v in FileStorage.__objects.items())
+            with open(FileStorage.__file_path) as file:
+                obj_data = json.load(file)
+                obj_data.update(new_data)
+                with open(FileStorage.__file_path, mode='w') as file:
+                    json.dump(new_data, file, indent=4)
+        else:
+            obj_data = dict((k, v.to_dict())
+                            for k, v in FileStorage.__objects.items())
+            with open(FileStorage.__file_path, mode='w') as file:
+                json.dump(obj_data, file, indent=4)
 
     def reload(self):
-        """
-        read from the file, deserialize the dict obtained from the file
-        each val from the dict is a dict of args and kwargs of an instance
-        each key found represent an instance we check if it
-        exit else we create new one
-        and append if to the __objects
-        :return: None
-        """
-        try:
-            with open(FileStorage.__file_path, mode="r") as file:
-                content = file.read()
-                dict_from_file = {}
-                if content != "":
-                    dict_from_file = json.loads(content)  # type = dict
+        """deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
-                for file_key, dict_obj in dict_from_file.items():
-                    # if the key_file is not in the storage.keys()
-                    # create a new instance and pass it argument and kwargs
-                    if file_key not in FileStorage.__objects.keys():
-                        className = dict_obj["__class__"]
-                        newInst = eval("{}(**dict_obj)".format(className))
-                        self.new(newInst)
-        except FileNotFoundError:
+        classes = {'BaseModel': BaseModel, 'User': User, 'State': State,
+                   'City': City, 'Amenity': Amenity, 'Place': Place,
+                   'Review': Review}
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path) as file:
+                json_str = json.load(file)
+            for k, v in json_str.items():
+                if v["__class__"] in classes:
+                    FileStorage.__objects[k] = classes[v["__class__"]](**v)
+        else:
             pass
